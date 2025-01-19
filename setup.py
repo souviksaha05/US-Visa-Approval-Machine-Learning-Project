242f9fb43730b195b602dea85f45dbc008831031
souviksaha05 <souvikriksaha@gmail.com>
2025-01-19 17:57:29 +0530

    mongoDB demo added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="US_Visa",
+    name="us_visa",
     version="0.0.0",
     author="Souvik",
     author_email="souvikriksaha@gmail.com",
